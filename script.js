1b8a20a9d3ab966efe1a5e795a2672ff35479d95
Captain <71038018+karimi65@users.noreply.github.com>
2021-07-06 23:08:22 -0500

    updated

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return;  // stop the function from running all together
-    audio.currentTime = 0; // rewind to the start
-    audio.play();
-    key.classList.add('playing')
-
-}
-
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return; // skip it if it's not a transform
     e.target.classList.remove('playing');
 }
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;  // stop the function from running all together
+
+    key.classList.add('playing')
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+
+}
+
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitioned', removeTransition));
